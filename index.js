46b497577c60a7c2abd32258baf3cce83079b51b
ayeshakhali <ayeshukhalid85@gmail.com>
2023-08-09 01:18:29 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,48 @@
+"use strict";
+//area of rectangle = length*width;
+var length = 15;
+var width = 10;
+var area = length * width;
+console.log(area);
+var length = 10;
+var width = 10;
+var height = 10;
+var volume = length ** 3;
+console.log(volume);
+//positive,negative or zero;
+var number1 = 54;
+// positive,negative or zero;
+var number1 = 54;
+// check if the number is greater than 0
+if (number1 > 0) {
+    console.log("The number is Positive.");
+}
+// check if number is 0
+else if (number1 == 0) {
+    console.log("The number is zero");
+}
+//check if the number is less than 0
+else {
+    console.log("The number is negative");
+}
+//number is even or odd
+var number1 = 0;
+if (number1 % 2 == 0) {
+    console.log("The number is even");
+}
+else {
+    console.log("The number is odd");
+}
+// write a program that  determine a person is eligible  to vote based or their age 
+var person_eligible = 21;
+// check eligiblity
+if (person_eligible >= 21) {
+    console.log("you are eligible for vote ");
+}
+else {
+    console.log("you are not eligible fore vote");
+}
+// ((10+5)*3-2)/(4%3)-7) find result 
+var quation_valve = (((10 + 5) * 3 - 2) / (4 % 3) - 7);
+// result of formala
+console.log("result of formal here,quation_value");
---
+++ index.ts
@@ -0,0 +1,64 @@
+//area of rectangle = length*width;
+
+var length:number = 15;
+var width:number = 10;
+
+var area:number =length*width;
+console.log(area);
+
+var length:number=10;
+var width:number=10;
+var height:number=10;
+ 
+var volume:number=length**3;
+console.log(volume);
+
+//positive,negative or zero;
+
+var number1:number=54;
+
+// positive,negative or zero;
+
+var number1:number=54;
+// check if the number is greater than 0
+ if (number1>0){
+ console.log("The number is Positive.");
+}
+// check if number is 0
+else if (number1==0){
+console.log("The number is zero");
+}
+//check if the number is less than 0
+else{
+console.log("The number is negative");
+}
+
+//number is even or odd
+
+var number1:number=0;
+if(number1%2==0){
+console.log("The number is even")
+}else{
+    console.log("The number is odd")
+}
+// write a program that  determine a person is eligible  to vote based or their age 
+
+var person_eligible:number= 21;
+
+// check eligiblity
+
+if (person_eligible >=21){
+    console.log("you are eligible for vote ")
+}
+else{
+    console.log("you are not eligible fore vote")
+}
+// ((10+5)*3-2)/(4%3)-7) find result 
+
+var quation_valve:number = (((10+5)*3-2)/(4%3)-7);
+
+// result of formala
+
+console.log("result of formal here,quation_value")
+
+
